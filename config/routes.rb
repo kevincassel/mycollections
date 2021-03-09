1460b6c1bbda9a4f1c264af75241e18fb467c32a
simon lacote <simonlacote@hotmail.fr>
2021-03-09 23:33:55 +0100

    add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources  :video_games , only: [:index, :show]
+  resources  :users , only: [:show]
   # resources :collections, only: [:index, :show]
 end
