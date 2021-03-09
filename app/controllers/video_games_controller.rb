cff1fc3812c50432b3550196ebc6d29fb24080e1
simon lacote <simonlacote@hotmail.fr>
2021-03-09 20:35:30 +0100

    init home page and game index

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -3,13 +3,11 @@ class VideoGamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @video_games = Video_game.all
+    @video_games = VideoGame.all
   end
 
   def show
-    @video_game = Video_game.find(params[:id])
+    @video_game = VideoGame.find(params[:id])
   end
-
-
 
 end
