14624f23b768a559d32655cc81dd8ca05510d7d0
kevincassel <kc.lewagon@gmail.com>
2021-03-10 12:19:43 +0100

    index shops: routes, controller, view, link_to, display cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Destroy all users "
+puts "Destroy all users"
 
 User.destroy_all
+
+puts "Destroy all shops"
+
+Shop.destroy_all
 
 puts "Create Users"
 
 User.create!(first_name: "Pierre", last_name: "Quiroule", username: "Plouf", email: "papapa@gmail.com", password:"papapa")
+
+puts "Create Shops"
+
+Shop.create!(name: "Game", address: "74, Rue Faidherbe, 59800 Lille")
+Shop.create!(name: "Micromania", address: "51 Avenue Willy Brandt, 59000 Lille")
+Shop.create!(name: "Micromania", address: "52 Rue de Béthune, 59800 Lille")
+Shop.create!(name: "Micromania", address: "130 Rue du Grand But Lomme, 59160 Lille")
+Shop.create!(name: "Micromania", address: "46 Avenue de l'Avenir, 59650 Villeneuve-d'Ascq")
 
 puts "Finished !"
