d47a72137d190722c872c10624f9826ccefa281a
simon lacote <simonlacote@hotmail.fr>
2021-03-10 16:15:42 +0100

    en cours

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -1,2 +1,25 @@
 class VideoGame < ApplicationRecord
+
+  require "uri"
+  require "net/http"
+
+  def searchagame
+  url = URI("https://api.igdb.com/v4/games")
+
+  https = Net::HTTP.new(url.host, url.port)
+  https.use_ssl = true
+
+  request = Net::HTTP::Post.new(url)
+  request["Client-ID"] = "60p43ul3st64mrthkneqpfofnevos4"
+  request["Authorization"] = "Bearer 5jbpjxhwzus3hh29qt936qu93tb7ip"
+  request["Content-Type"] = "text/plain"
+  request["Cookie"] = "__cfduid=d6058770d0ed32c11d8e9b48fe090c0811615375495"
+  request.body = "fields *;"
+
+  response = https.request(request)
+  @games = response.read_body
+  
+  end
+
+
 end
