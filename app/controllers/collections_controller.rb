2ca930eacae6d07163b92d1f3a307753e8fe6359
Théo Helsens <thelsens@hotmail.fr>
2021-03-11 13:50:20 +0100

    add game controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,19 @@
 class CollectionsController < ApplicationController
+
+  def create
+    @collection = Collection.new(collection_params)
+    @collection.user = current_user
+      if @collection.save
+        redirect_to @collection
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def stone_params
+    params.require(:collection).permit(:title, :platform, :editor, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources  :users , only: [:show]
   resources :shops, only: [:index]
   resources :messages, only: [:index, :show]
+  resources :collections, only: [:create, :new]
   # resources :collections, only: [:index, :show]
 end
