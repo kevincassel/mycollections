2a088c6f95d8ba95469179acd8e3cca7980aac0d
simon lacote <simonlacote@hotmail.fr>
2021-03-12 12:45:32 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_214138) do
+ActiveRecord::Schema.define(version: 2021_03_11_165548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "title"
@@ -34,9 +55,9 @@ ActiveRecord::Schema.define(version: 2021_03_10_214138) do
     t.bigint "user_id", null: false
     t.bigint "video_game_id", null: false
     t.string "user_comment"
+    t.bigint "shop_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "shop_id"
     t.index ["shop_id"], name: "index_collections_on_shop_id"
     t.index ["user_id"], name: "index_collections_on_user_id"
     t.index ["video_game_id"], name: "index_collections_on_video_game_id"
@@ -110,6 +131,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_214138) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "collection_tags", "collections"
   add_foreign_key "collection_tags", "tags"
   add_foreign_key "collections", "shops"
