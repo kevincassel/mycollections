abc568a63de034430acf725b05611896aeeb6756
kevincassel <kc.lewagon@gmail.com>
2021-03-16 15:47:23 +0100

    in progress

---
+++ app/javascript/plugins/init_quagga.js
@@ -0,0 +1,37 @@
+import Quagga from 'quagga';
+
+// Quagga.init will find on the dom the target
+// and will run a video with the camera (webcam on computer, camera on smartphone)
+const scan = () => {
+  const quagga = document.querySelector('#quagga')
+  if (quagga) {
+    Quagga.init({
+        inputStream : {
+          name : "Live",
+          type : "LiveStream",
+          target: quagga // <--- change '#yourElement' with your id
+        },
+        decoder : {
+          readers : ['ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader']
+        }
+      }, function(err) {
+          if (err) {
+              console.log(err);
+              return
+          }
+          console.log("Initialization finished. Ready to start");
+          Quagga.start();
+      });
+    
+    // the callback is called when a barcode is detected
+    // we can extract the barcode with data.codeResult.code
+    Quagga.onDetected((data) => {
+        const barcode = data.codeResult.code;
+        // TODO: do something with your backend or frontend
+        console.log(barcode)
+    
+        // you can call Quagga.stop() to stop the camera
+      });
+  }
+}
+  export { scan };
