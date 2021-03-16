ff65d8e4b90f4376de36e53ea20045471b39d421
kevincassel <kc.lewagon@gmail.com>
2021-03-16 02:39:01 +0100

    config api in collections controller finish

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -17,19 +17,35 @@ class CollectionsController < ApplicationController
     #cas classique d'un user qui ajoute un jeu via la db existante
     @collection = Collection.new
     games = VideoGame.all
-    raise
-    @collection.video_game = VideoGame.find(params[:video_game])
-    @collection.user = current_user
-    @collection.shop = Shop.first
-    redirect_to video_games_path(query: params[:query]) if @collection.save
-
+    if params[:video_game] != nil
+      @collection.video_game = VideoGame.find(params[:video_game])
+      @collection.user = current_user
+      @collection.shop = Shop.first
+      redirect_to video_games_path(query: params[:query]) if @collection.save
+    else
     #cas 2 creation via l'api
     # si params contient api_name
-    # si le jeu vidéo existe avec le nom dans params api_name
+    # si le jeu vidéo existe avec le nom dans params api_name dans la db
     # alors on crée la collection avec le jeu vidéo et le user
     # sinon appeler l'api et récupérer les infos et les mettres dans un hash
     # créer un nouveau jeu vidéo
     # créer une collection avec ce jeu.
+      game_title = params["game"]["name"] # nom du jeu recherché
+      game = games.select { |game| game.title.end_with?(game_title) }
+      if game != []
+        @collection.video_game = VideoGame.find(game.first.id)
+        @collection.user = current_user
+        @collection.shop = Shop.first
+        redirect_to new_collection_path(query: params[:query]) if @collection.save
+      else
+        game_info = {title: params["game"]["name"], platform: params["game"]["platforms"].first["name"], category: params["game"]["genres"].first["name"], description: params["game"]["summary"], picture: "https:#{params["game"]["cover"]["url"]}", year: params["game"]["first_release_date"] }
+        game_new = VideoGame.create(game_info)
+        @collection.video_game = VideoGame.find(game_new.id)
+        @collection.user = current_user
+        @collection.shop = Shop.first
+        redirect_to new_collection_path(query: params[:query]) if @collection.save
+      end
+    end
   end
 
   def destroy
