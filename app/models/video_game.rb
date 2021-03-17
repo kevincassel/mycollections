81b9285961b3d310a4e20d4eae50ce1bc149d894
Théo Helsens <thelsens@hotmail.fr>
2021-03-17 14:52:36 +0100

    show done 1

--- app/models/video_game.rb
+++ app/models/video_game.rb
@@ -3,22 +3,22 @@ class VideoGame < ApplicationRecord
   def self.searchagame(query)
     require "uri"
     require "net/http"
-    
+
     url = URI("https://api.igdb.com/v4/games")
-    
+
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
-    
+
     request = Net::HTTP::Post.new(url)
     request["Client-ID"] = "60p43ul3st64mrthkneqpfofnevos4"
     request["Authorization"] = "Bearer 5jbpjxhwzus3hh29qt936qu93tb7ip"
     request["Content-Type"] = "text/plain"
     request["Cookie"] = "__cfduid=d6058770d0ed32c11d8e9b48fe090c0811615375495"
     request.body = "search \"#{query}\"; fields name, platforms.name, first_release_date, summary, genres.name, cover.url;\r\n\r\n"
-    
+
     response = https.request(request)
     JSON.parse(response.read_body)
 
   end
-  
+
 end
