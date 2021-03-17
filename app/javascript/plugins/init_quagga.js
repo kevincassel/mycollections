743db828fb12a14e58fbf90be24d6339fb20febe
kevincassel <kc.lewagon@gmail.com>
2021-03-17 11:29:34 +0100

    return before delete proxy url scan for heroku

--- app/javascript/plugins/init_quagga.js
+++ app/javascript/plugins/init_quagga.js
@@ -30,7 +30,7 @@ const scan = () => {
         const api_key = process.env.BARCODE_API_KEY;
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
         // TODO: do something with your backend or frontend
-        const url = `https://api.barcodelookup.com/v2/products?barcode=${barcode}&formatted=y&key=` + api_key;
+        const url = proxyurl + `https://api.barcodelookup.com/v2/products?barcode=${barcode}&formatted=y&key=` + api_key;
         console.log(barcode);
         console.log(api_key);
         Quagga.stop();
