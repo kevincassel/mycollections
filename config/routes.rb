77aad5822a0777de494b8cae9b09ab575587b689
kevincassel <kc.lewagon@gmail.com>
2021-03-18 12:25:48 +0100

    change route scan

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :shops, only: [:index]
   resources :messages, only: [:index, :show]
-
+  get '/scan', to: 'messages#index', as: 'scan'
   resources :collections, only: [:index, :show, :new, :create, :destroy]
 
 end
